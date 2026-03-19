b3d667fbdb75c683112df12426d288cc58b0a847
Prajwal Kharode <prajwalkharode18@gmail.com>
2026-03-20 01:18:22 +0530

    Error handler

--- src/index3.js
+++ src/index3.js
@@ -15,12 +15,23 @@ app.post("/admin/deleteUser", (req, res) => {
 
 // For the below request handler middleware will not be called as middleware supports only /admin routes
 app.get("/user/userDetails", userAuthFunction, (req, res) => {
+    throw new Error(
+        "Thrown new error to check Error handler middelware's functionality",
+    );
     res.send("User details");
 });
 
 // No need to check user validation while user logging in so no need to pass middleware here for this route.
 app.post("/user/login", (req, res) => {
     res.send("User logged in successfully");
+});
+
+// To handler the errors in any of the route we should add a Error handler to handle errors coming from any route gracefully
+// This function should be writeen at the last (Routes Order matters).
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("Something went wrong returned error " + err);
+    }
 });
 
 app.listen(3002, () => {
