82a5ac735fd3ec4b8423e96d85f0f7cc2599f089
Prajwal Kharode <prajwalkharode18@gmail.com>
2026-03-19 19:50:10 +0530

    basic crud route handlers

--- index.js
+++ index.js
@@ -4,11 +4,38 @@ import express from "express";
 const app = express();
 
 // Request handlers:
-app.use("/", (req, res) => {
-    res.send("Response from dashboard");
+app.get("/users", (req, res) => {
+    res.send([
+        {
+            name: "User1",
+            gmail: "user1@gmail.com",
+        },
+    ]);
 });
 
-// app/ server is listening on port 3000 to handle incoming requests.
+app.post("/user", (req, res) => {
+    console.log(req);
+    res.send(req.body?.name);
+});
+
+app.put("/user", (req, res) => {
+    res.send("Updated user details");
+});
+
+app.patch("/user", (req, res) => {
+    res.send("Updated specific detail");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted Successfully");
+});
+
+// This will match all HTTP method API calls to / route for the routes comes after this
+app.use("/", (req, res) => {
+    res.send("Default Response if no routes matched");
+});
+
+// app/ server is listening  on port 3000 to handle incoming requests.
 app.listen(3000, () => {
     console.log("Application is running on port 3000");
 });
