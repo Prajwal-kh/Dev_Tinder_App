a9255ef1d0af3f1d199aa3d6e3ed711f3690d675
Prajwal Kharode <prajwalkharode18@gmail.com>
2026-03-21 00:03:46 +0530

    Dynamic data in /signup api

--- src/server.js
+++ src/server.js
@@ -8,14 +8,15 @@ const port = "7000";
 // Connect to the database before starting the server
 await connectDB();
 
+// To read the incoming JSON data and convert it into a JS object we need to pass a middleware to all routes
+// This will put it into the the req.body and give us access to read the data in JS object
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
+    const data = req.body;
+    console.log(data);
     // Creating new instance of User model
-    const user = new User({
-        firstName: "Pratik",
-        lastName: "Kharode",
-        email: "pratikk@gmail.com",
-        password: "pratikk4123",
-    });
+    const user = new User(req.body);
     try {
         await user.save();
         res.status(201).send("User added successfully to db");
