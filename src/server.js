3002d5fbb039b0cd0ad5c6c22bdcfa4968a9cbe7
Prajwal Kharode <prajwalkharode18@gmail.com>
2026-03-22 00:12:34 +0530

    Added new crud api's

--- src/server.js
+++ src/server.js
@@ -13,15 +13,74 @@ await connectDB();
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    const data = req.body;
-    console.log(data);
-    // Creating new instance of User model
     const user = new User(req.body);
     try {
         await user.save();
         res.status(201).send("User added successfully to db");
     } catch (error) {
         res.status(400).send("Error saving the user into db " + error);
+    }
+});
+
+app.get("/user/:email", async (req, res) => {
+    try {
+        const userEmailId = req.params.email;
+        const requestedUser = await User.findOne({ email: userEmailId });
+        if (!requestedUser) {
+            return res.status(404).send("User not found");
+        }
+
+        res.status(200).send(requestedUser);
+    } catch (error) {
+        res.status(400).send("Unable to get the list of all users " + error);
+    }
+});
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(400).send("Unable to get the list of all users " + error);
+    }
+});
+
+app.delete("/user/:id", async (req, res) => {
+    try {
+        const userId = req.params.id;
+        // If we want to delete a user by passing user id we can use findByIdAndDelete method
+        const deletedUser = await User.findByIdAndDelete(userId);
+        if (!deletedUser) {
+            return res
+                .status(400)
+                .send("Unable to delete user due to invalid user id");
+        }
+
+        res.send("User " + userId + " deleted Successfully");
+    } catch (error) {
+        res.status(400).send("Unable to delete user " + error);
+    }
+});
+
+app.patch("/user", async (req, res) => {
+    try {
+        const userId = req.body.userId;
+        const data = req.body;
+        // findByIdAndUpdate method works similar to findOneAndUpdate() if we are updating user by id
+        const updatedUser = await User.findByIdAndUpdate(userId, data, {
+            returnDocument: "after",
+        });
+        console.log("user new details: ", updatedUser);
+        if (!updatedUser) {
+            return res
+                .status(400)
+                .send("Unable to update user due to invalid user id");
+        }
+
+        res.send("User " + userId + " updated Successfully");
+    } catch (error) {
+        res.status(400).send("Unable to update user " + error);
     }
 });
 
